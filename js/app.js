7b9dd28623bce42ed98c9372a667095b11c01da4
alicia <alizunega84@gmail.com>
2021-10-25 17:36:30 -0300

    commit 25/10

--- js/app.js
+++ js/app.js
@@ -1,68 +1,71 @@
-var nombre = document.getElementById(nombre).value;
-var mail = document.getElementById(email).value;
-var phone = document.getElementById(phone).value;
+function validarCampos(formulario, archivo) {
+  //variables obtienen valores del formulario
+  var nombre = document.getElementById("nombre").value;
+  var telefono = document.getElementById("phone").value;
+  var mail = document.getElementById("email").value;
+  //creo variable de expresion regular para mail
+  var regExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-function validarFormulario(evento){
-  var expr = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/;
-  if ( !expr.test(mail) ){                                                           
-    alert("Error: La dirección de correo " + correo + " es incorrecta.");
-    return false;
-  }
-  
-  if(( nombre.lenght== 0 ) || (mail.lenght == 0) || ( phone == null)){
-    
-    alert("Este campo es obligatorio.");
-    return false;
+  var msjerror = "";
 
-  }else{
-    formulario.submi();
-    return true;
-  }  
-
-}
-
-
-
-function comprueba_extension(formulario, archivo) {
   extensiones_validas = new Array(".jpg", ".jpeg", ".png", ".svg");
   msjerror = "";
+
+  //verificacion de la extension del archivo
+  //primero evalua si hay o no archivo
+
   if (!archivo) {
-     //muestra mensaje si no se seleccionó algun archivo
-      msjerror = "No has seleccionado ningún archivo";
-  }else{
-     //recupero la extensión de este nombre de archivo
-     extension = (archivo.substring(archivo.lastIndexOf("."))).toLowerCase();
-     
-     //compruebo si la extensión está entre las permitidas
-     permitida = false;
-     for(var una of extensiones_validas){
-       if (una === extension){
-         permitida = true;
-         break;
-       } 
-     }
-     /*
-     for (var i = 0; i < extensiones_validas.length; i++) {
-        if (extensiones_validas[i] == extension) {
+    //muestra mensaje que no se ha seleccionado archivo
+    //envia el formulario sin archivo (es opcional)
+    msjerror = "El formulario se enviará sin archivo de muestra.";
+    alert(msjerror);
+
+  } else {
+    //recupero la extensión de este nombre de archivo
+    extension = (archivo.substring(archivo.lastIndexOf("."))).toLowerCase();
+
+    //compruebo si la extensión está entre las permitidas
+    permitida = false;
+    for (var una of extensiones_validas) {
+      if (una === extension) {
         permitida = true;
         break;
-        }
-     }*/
-     if (!permitida) {
-        msjerror = "Comprueba la extensión de los archivos a subir. \nSólo se pueden subir archivos con extensiones: " + extensiones_validas.join();
-      }else{
-         //extension permitida, se envia el formulario
-        alert("El formulario se envió con éxito.");
-        formulario.submit();
-        return 1;
       }
+    }
+    //evalua que mensaje muestra según la condición
+    if (permitida == false) {
+      msjerror = "Comprueba la extensión de los archivos a subir. \nSólo se pueden subir archivos con extensiones: " + extensiones_validas.join();
+      //muestra el mensaje de error correspondiente
+      alert(msjerror);
+      //no envia el formulario y sale (en teoria, Marge)
+      return 0;
+
+    }
   }
-  //muestra el mensaje de error correspondiente
-  alert (msjerror);
-  return 0;
+
+  //verificar si campos vacios
+  //si los hay avisa que son obligatorios
+
+  if (nombre === "" && (telefono === null || mail === "")) {
+    alert("Hay campos obligatorios vacíos.");
+    
+
+    //comprobamos si el email tiene formato correcto
+
+  } else if (!regExp.test(mail)) {
+    alert("Ingrese un formato de email válido. \nEjemplo: ejemplo@mail.com");
+    
+
+  } else {
+    //todo OK
+    alert("Su formulario ha sido enviado exitosamente.")
+    
+    return 1;
+  }
+return false;
+
 }
 
 
 
 
-  
